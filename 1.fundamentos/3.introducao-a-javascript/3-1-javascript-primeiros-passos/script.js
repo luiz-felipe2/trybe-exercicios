e123602f046059679dfea888e7a9ac5ae5374753
Luiz Felipe <luiz.felipe213@gmail.com>
2023-01-23 22:00:40 -0300

    andamento do exercicio

---
+++ 1.fundamentos/3.introducao-a-javascript/3-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,52 @@
+// const a = 10;
+// const b = 5;
+
+// const adicao = a + b;
+// console.log(adicao);
+
+// const subtracao = a - b;
+// console.log(subtracao);
+
+// const multiplicacao = a * b;
+// console.log(multiplicacao);
+
+// const divisao = a / b;
+// console.log(divisao);
+
+// const modulo = a % b;
+// console.log(modulo);
+
+// const a = 10;
+// const b = 11;
+
+// if (a > b) {
+//     console.log("'a' é maior que 'b'")
+// } else {
+//     console.log("'b' é maior que 'a'")
+// };
+
+// const a = 10;
+// const b = 11;
+// const c = 12;
+
+// if (a > b & c) {
+//     console.log("'a' é maior que 'b' e 'c'")
+// } else if ( b > a & c) {
+//     console.log("'b' é maior que 'a' e 'c'")
+// } else {
+//     console.log("'c' é maior que 'a' e 'b'")
+// };
+
+
+const valor = 4;
+
+    if (valor > 0) {
+        console.log("positive");
+    }   else if (valor < 0) {
+        console.log("negative");
+    }   else {
+        console.log("zero");
+    }  
+
+
+
